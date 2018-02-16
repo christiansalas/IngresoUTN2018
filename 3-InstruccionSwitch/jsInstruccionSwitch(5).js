4b9df95c36d683a27603d1435a626f2f70f4964f
christiansalas <36246843+christiansalas@users.noreply.github.com>
2018-02-16 14:56:55 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -2,9 +2,30 @@ function Mostrar()
 {
 //tomo la hora 
 var laHora = document.getElementById('hora').value;
+switch (laHora)
 
-//alert (laHora);
-	
+{
+case "7":
+case "8":
+case "9":
+case "10":
+case "11":
+ alert ("Es de mañana");break;
+ 
+
+ 
+
+ 
+ }
+ 
+
+
+
+
+
+
+
+
 	
 
 
